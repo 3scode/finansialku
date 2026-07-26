9d1eb9acf6b9fb551b1ea42339388c89963ccf82
TrisnoSanjaya <email.trisno.sanjaya@gmail.com>
2026-07-26 13:24:26 +0700

    feat: sesuaikan ikon Android & splash screen dengan favicon website (wallet/peach)

--- android/app/src/main/java/com/finansialku/app/MainActivity.java
+++ android/app/src/main/java/com/finansialku/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.finansialku.app;
 
+import android.os.Build;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().setDecorFitsSystemWindows(false);
+        }
+    }
+}
